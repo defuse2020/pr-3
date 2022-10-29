894b5bc78d7f1594bb9f858c241a1a13d84a2caf
z36123@outlook.com <z36123@outlook.com>
2022-10-29 09:26:30 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public delegate void AccountHandler(string message);
+    public class AccClass
+    {
+        int sum;
+        string fio;
+        AccountHandler taken;
+        public AccClass(int sum, string fio)
+        {
+            this.sum = sum;
+            this.fio = fio;
+        }
+        // Регистрируем делегат
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken = del;
+        }
+        public void Add(int sum) => this.sum += sum;
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                taken?.Invoke($"Со счета списано {sum} у.е.");
+            }
+            else
+            {
+                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        AccClass acc;
+        class Denga
+        {
+
+        }
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AccClass acc = new AccClass(Convert.ToInt32(textBox2.Text), textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            acc.Add(Convert.ToInt32(textBox3.Text));
+            listBox1.Items.Add(acc);
+        }
+    }
+}
